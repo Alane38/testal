ccd4315984f0286982b7e477f708cb2ed5fc5c06
Alane38 <52476035+Alane38@users.noreply.github.com>
2022-05-19 13:24:59 +0200

    Fix structure, style, adding command select & input

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Animated, SafeAreaView, TextInput } from 'react-native';
-import { Entypo } from '@expo/vector-icons';
+import { View, Text, StyleSheet, TouchableOpacity, Animated, TextInput, Button, Dimensions, FlatList } from 'react-native';
+import { Entypo, FontAwesome } from '@expo/vector-icons';
 
 // import { Switch } from 'react-native-switch';
 import Switch from './component/Switch';
 
 const App = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
+  const [isDark, setIsDark] = useState(false);
 
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const toggleSwitch = () => setIsDark((previousState) => !previousState);
 
   const FadeInView = (props) => {
     const fadeAnim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
@@ -36,72 +36,129 @@ const App = () => {
     );
   }
 
-  const [text, onChangeText] = React.useState("Useless Text");
-  const [number, onChangeNumber] = React.useState(null);
+  const [command, setCommand] = React.useState("")
+
+  const executeCommand = () => {
+    if (command === '/light') {
+      setIsDark(false)
+      // truc
+    } else if (command === '/dark') {
+      setIsDark(true)
+    }
+    setCommand('')
+  }
+
+  const directExecCommand = () =>{
+    setCommand(item.item.command)
+    executeCommand()
+  }
+  // const [number, onChangeNumber] = React.useState(null);
 
   // const switchObject = () => {
   //   toggleSwitch();
   //   FadeInView();
   // }
 
+  var commands = [
+    {
+      id: 1,
+      icon: 'moon-o',
+      name: 'Dark mode',
+      description: 'Enable dark mode',
+      command: '/dark'
+    },
+    {
+      id: 2,
+      icon: 'test',
+      name: 'Light mode',
+      description: 'Enable light mode',
+      command: '/light'
+    }
+  ]
+
   return (
-    <View style={isEnabled ? styles.blackContainer : styles.defaultContainer}>
-      <Switch
-        barHeight={30}
-        switchWidth={40}
-        switchHeight={20}
-        value={isEnabled}
-        onValueChange={toggleSwitch}
-        disabled={false}
-        backgroundActive={'#0095ff'}
-        backgroundInactive={'#d1d1d1'}
-        circleActiveColor={'white'}
-        circleInActiveColor={'white'}
-        // renderInsideCircle={() => <CustomComponent />} // Composant personnalisé à rendre à l'intérieur du cercle de commutation (texte, image, etc.)
-        changeValueImmediately={true} // Si vous rendez à l'intérieur du cercle, modifiez immédiatement l'état ou attendez que l'animation se termine
-        innerCircleStyle={{
-          borderWidth: 0,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }} // Style pour le cercle animé intérieur pour ce que vous (peut) rendre à l'intérieur du cercle
-        outerCircleStyle={{}} // style pour le cercle animé extérieur
-        renderActiveText={false}
-        renderInActiveText={false}
-        switchLeftPx={3} // dénominateur pour la logique lors de la glissation à la vraie position.Nombre plus élevé = plus d'espace de la droite du cercle à la fin du curseur
-        switchRightPx={3} // dénominateur pour la logique lors de la glissement en fausse position.Nombre plus élevé = plus d'espace de gauche du cercle au début du curseur
-        switchWidthMultiplier={2} // Multiplié par l'accessoire «CirclEze» pour calculer la largeur totale du commutateur
-        switchBorderRadius={30} // Définit le rayon de bordure du curseur de l'interrupteur.S'il est non set, il reste la circleze.
-      />
-      <Text>{'\n'}</Text>
-      <TouchableOpacity
-        onPress={toggleSwitch}
-        style={{
-          backgroundColor: isEnabled ? '#0095ff' : '#d1d1d1',
-          borderRadius: 100,
-          padding: 10,
-        }}>
-        <Entypo
-          name={isEnabled ? 'light-up' : 'moon'}
-          size={25}
-          color="white"
-        />
-      </TouchableOpacity>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
-        <FadeInView style={isEnabled ? styles.enableSwitchBoxFadeView : styles.disableSwitchBoxFadeView}>
-          <Text style={isEnabled ? styles.enableSwitchBoxFadeViewText : styles.disableSwitchBoxFadeViewText}>{isEnabled ? 'Activé' : 'Désactivé'}</Text>
-        </FadeInView>
-      </View>
-      <View style={{ bottom: 1, width: 400, height: 580, backgroundColor: '#000' }}>
-        <View style={{
-          position: 'absolute', right: 0, bottom: 1, width: 100, height: 50, padding: 30, height: 50, width: 400, margin: 0, backgroundColor: '#fff'
-        }}>
-          <TextInput
-            style={styles.input}
-            onChangeText={onChangeNumber}
-            value={number}
-            keyboardType="default"
+    <View style={isDark ? styles.blackContainer : styles.defaultContainer}>
+      <View style={{ alignItems: 'center' }}>
+        <View style={{ marginBottom: 15 }}>
+          <Switch
+            barHeight={30}
+            switchWidth={40}
+            switchHeight={20}
+            value={isDark}
+            onValueChange={toggleSwitch}
+            disabled={false}
+            backgroundActive={'#0095ff'}
+            backgroundInactive={'#d1d1d1'}
+            circleActiveColor={'white'}
+            circleInActiveColor={'white'}
+            // renderInsideCircle={() => <CustomComponent />} // Composant personnalisé à rendre à l'intérieur du cercle de commutation (texte, image, etc.)
+            changeValueImmediately={true} // Si vous rendez à l'intérieur du cercle, modifiez immédiatement l'état ou attendez que l'animation se termine
+            innerCircleStyle={{
+              borderWidth: 0,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }} // Style pour le cercle animé intérieur pour ce que vous (peut) rendre à l'intérieur du cercle
+            outerCircleStyle={{}} // style pour le cercle animé extérieur
+            renderActiveText={false}
+            renderInActiveText={false}
+            switchLeftPx={3} // dénominateur pour la logique lors de la glissation à la vraie position.Nombre plus élevé = plus d'espace de la droite du cercle à la fin du curseur
+            switchRightPx={3} // dénominateur pour la logique lors de la glissement en fausse position.Nombre plus élevé = plus d'espace de gauche du cercle au début du curseur
+            switchWidthMultiplier={2} // Multiplié par l'accessoire «CirclEze» pour calculer la largeur totale du commutateur
+            switchBorderRadius={30} // Définit le rayon de bordure du curseur de l'interrupteur.S'il est non set, il reste la circleze.
           />
         </View>
+        <TouchableOpacity
+          onPress={toggleSwitch}
+          style={{
+            backgroundColor: isDark ? '#0095ff' : '#d1d1d1',
+            borderRadius: 100,
+            height: 50,
+            width: 50,
+            padding: 10,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+          <Entypo
+            name={isDark ? 'light-up' : 'moon'}
+            size={25}
+            color="white"
+          />
+        </TouchableOpacity>
+
+        <FadeInView style={isDark ? styles.enableSwitchBoxFadeView : styles.disableSwitchBoxFadeView}>
+          <Text style={isDark ? styles.enableSwitchBoxFadeViewText : styles.disableSwitchBoxFadeViewText}>{isDark ? 'Activé' : 'Désactivé'}</Text>
+        </FadeInView>
+      </View>
+
+      <View style={{ backgroundColor: '#000', width: Dimensions.get('window').width, height: '100%', padding: 20 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+          <TextInput
+            style={[styles.input, { flex: 1, marginRight: 15 }]}
+            onChangeText={(value) => setCommand(value)}
+            onPressOut={() => setCommand('/')}
+            value={command}
+            placeholder="Commande"
+            keyboardType="default"
+          />
+          <TouchableOpacity onPress={() => executeCommand()} style={{ height: 35, width: 35, borderRadius: 100, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' }}>
+            <FontAwesome name="paper-plane" size={16} color="black" style={{ marginLeft: -2 }} />
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          data={commands}
+          keyExtractor={item => item.id}
+          renderItem={(item) => {
+            return (
+              <TouchableOpacity onPress={() => directExecCommand()} style={{ padding: 10, flexDirection: 'row', alignItems:'center' }}>
+                <FontAwesome name={item.item.icon} size={27} color="#fff" style={{ marginRight: 10 }} />
+                <View>
+                  <Text style={{ color: '#fff', fontWeight: 'bold' }}>{item.item.name}</Text>
+                  <Text style={{ color: '#fff' }}>{item.item.description}</Text>
+                </View>
+              </TouchableOpacity>
+            )
+          }}
+        />
       </View>
     </View>
   );
@@ -110,13 +167,13 @@ const App = () => {
 const styles = StyleSheet.create({
   defaultContainer: {
     padding: 20,
-    flex: 1,
+    justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: "#fff"
   },
   blackContainer: {
     padding: 20,
-    flex: 1,
+    justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: '#000'
   },
@@ -143,10 +200,12 @@ const styles = StyleSheet.create({
     margin: 10
   },
   input: {
-    color: "#fff",
-    backgroundColor: "#000",
-    fontSize: 54,
-    margin: -27,
+    color: "#000",
+    backgroundColor: "#fff",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 7,
+    fontSize: 20,
   }
 });
 
